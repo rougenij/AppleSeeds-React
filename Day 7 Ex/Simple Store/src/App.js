161907c588d7501d356a9f1554b7ece895434774
rougenij <rougenij@gmail.com>
2021-12-21 12:09:20 +0200

    Added 404 page

--- Day 7 Ex/Simple Store/src/App.js
+++ Day 7 Ex/Simple Store/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./style.css";
 import HomePage from "./components/HomePage/HomePage";
 import Products from "./components/Products/Products";
 import Header from "./components/Header/Header";
 import ProductDetails from "./components/ProductDetails";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
@@ -16,6 +17,10 @@ function App() {
           <Route path="/products" exact component={Products} />
           <Route path="/product/:id" exact component={ProductDetails} />
         </div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route component={NotFound} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
