ce6a9fa510a1561af37316932bfd50fb72740429
rougenij <rougenij@gmail.com>
2021-12-21 12:08:23 +0200

    Added a 404 Page

---
+++ my-app/src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div>
+      <p style={{ fontSize: "5rem", color: "red", textAlign: "center" }}>
+        Nice Try accessing an unavaiable website buddy
+      </p>
+    </div>
+  );
+}
+
+export default NotFound;
