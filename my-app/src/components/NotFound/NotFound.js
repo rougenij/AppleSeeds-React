abf2d416b0c5c3813fc7a07860bc944546b52ae0
rougenij <rougenij@gmail.com>
2021-12-21 12:09:49 +0200

    Fixed the NotFound

--- my-app/src/components/NotFound/NotFound.js
+++ my-app/src/components/NotFound/NotFound.js
@@ -4,7 +4,7 @@ function NotFound() {
   return (
     <div>
       <p style={{ fontSize: "5rem", color: "red", textAlign: "center" }}>
-        Nice Try accessing an unavaiable website buddy
+        Nice Try accessing an unavaiable page buddy
       </p>
     </div>
   );
