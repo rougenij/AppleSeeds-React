a71aa71ac6072e799dc1ae52f02de8e669483d74
Rouge Nij <rougenij@gmail.com>
2021-12-26 12:15:45 +0200

    Finished First 3 questions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,20 @@
-import React from "react";
-import ToDoList from "./components/ToDoList";
-import "./App.css";
+import ToDoList from "./ToDoList";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <ToDoList />
-      </div>
-    );
-  }
+function App() {
+  const arr = [
+    { name: "CSS", completed: true },
+    { name: "JavaScript", completed: true },
+    { name: "Learn React", completed: false },
+    { name: "Learn mongoDB", completed: false },
+    { name: "Learn Node JS", completed: false },
+  ];
+
+  const displayList = () => {
+    return arr.map((item) => {
+      return <ToDoList text={item.name} completed={item.completed} />;
+    });
+  };
+  return <div className="App">{displayList()}</div>;
 }
 
 export default App;
