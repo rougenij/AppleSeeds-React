b8171173a27e5fac83587b2451ac26e2f4edff61
Rouge Nij <rougenij@gmail.com>
2021-12-29 18:00:39 +0200

    Mid Project

---
+++ MidWayProject.jsx
@@ -0,0 +1,52 @@
+//****************************************** Super Mario ***********************************************/
+
+//! The API ->
+// Create my own mockAPI for high score users --> basically type a new name everytime you start the game. and at the end push the highscore? but how im gonna implement a highscore? and for what
+
+//Crud? still no idea where to use? maybe to remove and edit old users
+
+//! Pages ->
+// Homepage + Rules --> Says the rules of the game and how to beat it? --> Map of the highscores
+//First world ---> Game
+//Second World?? --> Going down a pipe to a new identical but different world??? I guess
+//Page for users --> Like an Admin Panel
+
+
+//! Functions to have :
+
+// DrawMap function --> Canvas or grid?? --> React
+// Player function --> Includes a left and right moving function with jump
+// Monster Function --> moving left and right
+// Pipe --> going to another level function
+
+//! Thinks i need to figure out :
+//? Hit the box above to get a special gift?????????????
+//? What happens when a monstar and Mario hit each other
+//? Learn about Canvas with React
+//? useContext looks helpful in this project. according to links saved down below :
+
+//! Things to watch / read :
+
+//? Canvas ->
+//https://levelup.gitconnected.com/rpg-game-with-react-redux-html5-part-1-build-a-tile-map-9144fd867830
+//https://medium.com/@pdx.lucasm/canvas-with-react-js-32e133c05258
+//https://blog.cloudboost.io/using-html5-canvas-with-react-ff7d93f5dc76
+
+//? useContext ->
+//https://www.youtube.com/watch?v=35lXWvCuM8o&list=PL4eYoNxGhrACypGF0RkMLZOr7_8v0L8vf&index=2&ab_channel=DevEd
+
+
+//? Two Way Auth --> Basically with google
+// Auth 2.0 
+//! Google sign-in Auth API + MockAPI to save data for the users. --> Player name / Google name and his score
+
+
+
+//! With Pini
+
+//Two stage auth -> 2 Ways to lock in -> Unique username Ex: "Pini" -> This will add Pini in the mockApi
+// And if its already in the API it says user already exists
+// Sound Effects for Mario
+
+//! Player can play if he is logged in or not -> If not
+
